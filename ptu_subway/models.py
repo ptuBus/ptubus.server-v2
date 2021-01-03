cc707d1b39702ed2308462fb5c05654d90c0c4b2
RokHyungSon <fhrgud1264@gmail.com>
2021-01-04 01:13:20 +0900

    ptu_subway: ptu_subway 모델 추가

--- ptu_subway/models.py
+++ ptu_subway/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class SubwayLine(models.Model):
+    lineName = models.CharField(max_length=100)
+    lineCode = models.CharField(max_length=100)
+    lineColorCode = models.CharField(max_length=100)
+    lineSaidName = models.CharField(max_length=100, null=True)
+
+class SubwayStation(models.Model):
+    stationName = models.CharField(max_length=100)
+    stationCode = models.CharField(max_length=100)
+    lineCode = models.CharField(max_length=100)
+    railLineCode = models.CharField(max_length=100)
