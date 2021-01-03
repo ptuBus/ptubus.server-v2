04b5ab7af3562a09a2d8da2f7dea6b3dc783aff9
RokHyungSon <fhrgud1264@gmail.com>
2021-01-03 21:20:01 +0900

    ptu_subway: ptu_subway 앱 추가

--- configs/settings.py
+++ configs/settings.py
@@ -57,6 +57,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    #For ptuBus_Server
+    'ptu_subway',
 ]
 
 MIDDLEWARE = [
---
+++ ptu_subway/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PtuSubwayConfig(AppConfig):
+    name = 'ptu_subway'
