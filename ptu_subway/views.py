79489a23ac04500c434f88b3b8d5f4b61f0d322f
RokHyungSon <fhrgud1264@gmail.com>
2021-01-10 04:15:10 +0900

    ptu_subway: Crawling을 해올때 model이 누적되는 문제 수정

--- ptu_subway/views.py
+++ ptu_subway/views.py
@@ -4,7 +4,14 @@ from rest_framework.permissions import (
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from ptu_subway.serializer import SubwayLineSerializer, SubwayStationSerializer
+from ptu_subway.models import (
+    SubwayLine,
+    SubwayStation,
+)
+from ptu_subway.serializer import (
+    SubwayLineSerializer,
+    SubwayStationSerializer,
+)
 from ptu_subway import (
     get_line_data,
     get_station_data,
@@ -13,11 +20,15 @@ from ptu_subway import (
 
 class CrawlingSubwayLineData(APIView):
     permission_classes = [IsAdminUser]
+    queryset = SubwayLine.objects.all()
 
     def get(self, request, **kwargs):
+        self.queryset.delete()
+
         line_data = get_line_data(
             "select LINE_NAME,LINE_CODE,LINE_COLOR_CODE, GUGTOBU_LINE_NAME from LINE_DATA WHERE AREA_CD='CA'"
         )
+
         serializer = SubwayLineSerializer(
             data=line_data,
             many=True,
@@ -33,9 +44,13 @@ class CrawlingSubwayLineData(APIView):
 
 class CrawlingSubwayStationData(APIView):
     permission_classes = [IsAdminUser]
+    queryset = SubwayStation.objects.all()
 
     def get(self, request, **kwargs):
+        self.queryset.delete()
+
         station_data = get_station_data()
+
         serializer = SubwayStationSerializer(
             data=station_data,
             many=True,
@@ -43,6 +58,7 @@ class CrawlingSubwayStationData(APIView):
         )
         serializer.is_valid(raise_exception=True)
         serializer.save()
+
         return Response(
             {"SubwayStation": "데이터가 CSV에 의하여 생성되었습니다."},
         )
