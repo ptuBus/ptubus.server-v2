bae2900c2973e871158c0c857e74fb74c5568f9d
RokHyungSon <fhrgud1264@gmail.com>
2021-01-17 03:41:54 +0900

    ptu_bus: v1에 기반한 모델 추가

--- ptu_bus/admin.py
+++ ptu_bus/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from ptu_bus.models import (
+    BusTerminal,
+    BusTimeTable,
+)
 
-# Register your models here.
+
+@admin.register(BusTerminal)
+class BusTerminalAdmin(admin.ModelAdmin):
+    fields = (
+        "start_station_name",
+        "start_station_id",
+        "end_station_name",
+        "end_station_id",
+        "is_express",
+    )
+    list_display = (
+        "start_station_name",
+        "start_station_id",
+        "end_station_name",
+        "end_station_id",
+        "is_express",
+    )
+
+
+@admin.register(BusTimeTable)
+class BusTimeTableAdmin(admin.ModelAdmin):
+    fields = (
+        "bus_terminal",
+        "waste_time",
+        "normal_fare",
+        "special_fare",
+        "night_fare",
+        "schedule",
+        "night_schedule",
+    )
+    list_display = (
+        "bus_terminal",
+        "waste_time",
+        "normal_fare",
+        "special_fare",
+        "night_fare",
+        "schedule",
+        "night_schedule",
+    )
--- ptu_bus/models.py
+++ ptu_bus/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class BusTerminal(models.Model):
+    start_station_name = models.CharField(max_length=100)
+    start_station_id = models.CharField(max_length=100)
+    end_station_name = models.CharField(max_length=100)
+    end_station_id = models.CharField(max_length=100)
+    is_express = models.IntegerField()
+
+
+class BusTimeTable(models.Model):
+    bus_terminal = models.ForeignKey(
+        BusTerminal,
+        related_name="related_bus_timetable",
+        on_delete=models.CASCADE,
+    )
+    waste_time = models.CharField(max_length=100)
+    normal_fare = models.CharField(max_length=100)
+    special_fare = models.CharField(max_length=100)
+    night_fare = models.CharField(max_length=100)
+    schedule = models.CharField(max_length=100)
+    night_schedule = models.CharField(max_length=100)
