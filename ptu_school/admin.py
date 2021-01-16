4cfa21296f4b6511c43f6fc0117543ca08f59dad
RokHyungSon <fhrgud1264@gmail.com>
2021-01-17 03:53:56 +0900

    ptu_school: api v1에 기반하여 모델 추가

--- ptu_school/admin.py
+++ ptu_school/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from ptu_school.models import SchoolBusTimeTable
 
-# Register your models here.
+
+@admin.register(SchoolBusTimeTable)
+class SchoolBusTimeTableAdmin(admin.ModelAdmin):
+    fields = (
+        "start_station_name",
+        "start_station_id",
+        "end_station_name",
+        "end_station_id",
+        "schedule",
+        "up_down_type_code",
+    )
+    list_display = (
+        "start_station_name",
+        "start_station_id",
+        "end_station_name",
+        "end_station_id",
+        "schedule",
+        "up_down_type_code",
+    )
--- ptu_school/models.py
+++ ptu_school/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class SchoolBusTimeTable(models.Model):
+    start_station_name = models.CharField(max_length=100)
+    start_station_id = models.CharField(max_length=100)
+    end_station_name = models.CharField(max_length=100)
+    end_station_id = models.CharField(max_length=100)
+    schedule = models.CharField(max_length=100)
+    up_down_type_code = models.CharField(max_length=20)
