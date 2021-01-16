79e053803d96563127d46631d4655b3f6a86a6bb
RokHyungSon <fhrgud1264@gmail.com>
2021-01-17 04:00:12 +0900

    ptu_subway: api v1에 기반하여 모델 생성

--- ptu_train/admin.py
+++ ptu_train/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from ptu_train.models import (
+    TrainStation,
+    TrainTimeTable,
+)
 
-# Register your models here.
+
+@admin.register(TrainStation)
+class TrainStationAdmin(admin.ModelAdmin):
+    fields = (
+        "start_station_name",
+        "start_station_id",
+        "end_station_name",
+        "end_station_id",
+    )
+    list_display = (
+        "start_station_name",
+        "start_station_id",
+        "end_station_name",
+        "end_station_id",
+    )
+
+
+@admin.register(TrainTimeTable)
+class TrainTimeTableAdmin(admin.ModelAdmin):
+    fields = (
+        "train_station",
+        "rail_name",
+        "train_class",
+        "departure_time",
+        "schedule",
+        "waste_time",
+        "daily_type_code",
+    )
+    list_display = (
+        "train_station",
+        "rail_name",
+        "train_class",
+        "departure_time",
+        "schedule",
+        "waste_time",
+        "daily_type_code",
+    )
--- ptu_train/models.py
+++ ptu_train/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class TrainStation(models.Model):
+    start_station_name = models.CharField(max_length=100)
+    start_station_id = models.CharField(max_length=100)
+    end_station_name = models.CharField(max_length=100)
+    end_station_id = models.CharField(max_length=100)
+
+
+class TrainTimeTable(models.Model):
+    train_station = models.ForeignKey(
+        TrainStation,
+        related_name="related_train_timetable",
+        on_delete=models.CASCADE,
+    )
+    rail_name = models.CharField(max_length=100)
+    train_class = models.CharField(max_length=100)
+    departure_time = models.CharField(max_length=100)
+    schedule = models.CharField(max_length=100)
+    waste_time = models.CharField(max_length=100)
+    daily_type_code = models.CharField(max_length=100)
