beadee3cf6fc250d5cd8334301f1449a274b5914
RokHyungSon <fhrgud1264@gmail.com>
2021-02-07 20:59:44 +0900

    crawler: BusTimeTableCrawler 추가

--- foundation/crawler.py
+++ foundation/crawler.py
@@ -1,8 +1,12 @@
 import json
 from urllib import parse, request
 
+import django
+
+django.setup()
+
 from foundation import get_api_key
-from ptu_bus.models import BusTerminal
+from ptu_bus.models import BusTerminal, BusTimeTable
 
 
 class BaseCrawler:
@@ -63,5 +67,65 @@ class BusTerminalCrawler(BaseCrawler):
                             ).save()
 
 
+class BusTimeTableCrawler(BaseCrawler):
+    def __init__(self):
+        super().__init__()
+        self.bus_terminal_data = BusTerminal.objects.all()
+        self.url = [
+            "https://api.odsay.com/v1/api/intercityServiceTime?",
+            "https://api.odsay.com/v1/api/expressServiceTime?",
+        ]
+
+    def clean_schedule(self, schedule):
+        replace_all = schedule.replace("/", " ")
+        return "".join(replace_all).split()
+
+    def min_2_hour(self, time):
+        if time.find(":") == -1:
+            time = int(time)
+            hour = time // 60
+            min = time % 60
+            return str(hour).zfill(2) + ":" + str(min).zfill(2)
+        else:
+            return time
+
+    def collect_data(self):
+        for bus_terminal in self.bus_terminal_data:
+            url = self.url[bus_terminal.is_express]
+
+            self.query = [
+                ("apiKey", self.api_key),
+                ("startStationID", bus_terminal.start_station_id),
+                ("endStationID", bus_terminal.end_station_id),
+            ]
+
+            odsay_data = self.open_url(url)
+
+            bus_timetable_data = odsay_data["result"]["station"][0]
+            schedules = self.clean_schedule(bus_timetable_data["schedule"])
+
+            if bus_timetable_data.get("specialFare"):
+                special_fare = bus_timetable_data["specialFare"]
+            else:
+                special_fare = 0
+
+            if not bus_timetable_data["nightSchedule"]:
+                night_schedule = bus_timetable_data["nightSchedule"]
+            else:
+                night_schedule = "0"
+
+            for schedule in schedules:
+                BusTimeTable(
+                    bus_terminal=bus_terminal,
+                    waste_time=self.min_2_hour(bus_timetable_data["wasteTime"]),
+                    normal_fare=bus_timetable_data["normalFare"],
+                    special_fare=special_fare,
+                    night_fare=bus_timetable_data["nightFare"],
+                    schedule=schedule,
+                    night_schedule=night_schedule,
+                ).save()
+
+
 if __name__ == "__main__":
-    print(BusTerminalCrawler().collect_data())
+    BusTerminalCrawler().collect_data()
+    print(BusTimeTableCrawler().collect_data())
