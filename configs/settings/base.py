b76beccafe70f4a979c5deef8fa076f48a39c71c
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-02-13 00:08:25 +0900

    configs: base.py - ALLOWED_HOST 삭제 / dev.py - ALLOWED_HOST 변경

--- configs/settings/base.py
+++ configs/settings/base.py
@@ -29,9 +29,6 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
