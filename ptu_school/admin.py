39c6ac976945185557258061258276c712e17d9c
RokHyungSon <fhrgud1264@gmail.com>
2021-03-22 00:59:20 +0900

    ptu_school: pk를 제외한 데이터를 관리할 수 있는 key 필드 추가

--- ptu_school/admin.py
+++ ptu_school/admin.py
@@ -5,6 +5,7 @@ from ptu_school.models import SchoolBusTimeTable
 @admin.register(SchoolBusTimeTable)
 class SchoolBusTimeTableAdmin(admin.ModelAdmin):
     fields = (
+        "key",
         "start_station_name",
         "start_station_id",
         "end_station_name",
@@ -13,6 +14,7 @@ class SchoolBusTimeTableAdmin(admin.ModelAdmin):
         "up_down_type_code",
     )
     list_display = (
+        "key",
         "start_station_name",
         "start_station_id",
         "end_station_name",
--- ptu_school/models.py
+++ ptu_school/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class SchoolBusTimeTable(models.Model):
+    key = models.IntegerField(
+        default=None,
+        editable=False,
+    )
     start_station_name = models.CharField(max_length=100)
     start_station_id = models.CharField(max_length=100)
     end_station_name = models.CharField(max_length=100)
