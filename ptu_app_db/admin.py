a7be1c57e6b3584cab9e7ec5734784d764a541d3
RokHyungSon <fhrgud1264@gmail.com>
2021-03-22 00:58:07 +0900

    ptu_app_db: appdb에 고유한 uuid 필드 추가

--- ptu_app_db/admin.py
+++ ptu_app_db/admin.py
@@ -5,4 +5,7 @@ from ptu_app_db.models import AppDB
 @admin.register(AppDB)
 class AppDBAdmin(admin.ModelAdmin):
     fields = ("file_key",)
-    list_display = ("file_key",)
+    list_display = (
+        "uuid",
+        "file_key",
+    )
--- ptu_app_db/models.py
+++ ptu_app_db/models.py
@@ -1,5 +1,11 @@
+import uuid as uuid
 from django.db import models
 
 
 class AppDB(models.Model):
+    uuid = models.UUIDField(
+        unique=True,
+        default=uuid.uuid4,
+        editable=False,
+    )
     file_key = models.CharField(max_length=100)
