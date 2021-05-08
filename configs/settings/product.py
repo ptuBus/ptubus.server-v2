fbf14608f126735c55ee365e4c2657f46df8e908
RokHyungSon <fhrgud1264@gmail.com>
2021-05-09 04:30:16 +0900

    feat(crontab): crawler를 crontab에 등록

--- configs/settings/product.py
+++ configs/settings/product.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split()
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
@@ -20,3 +20,11 @@ DATABASES = {
 }
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+CRONJOBS = [
+    ("0 1 * * *", "foundation.crawler.BusTerminalCrawler().collect_data()"),
+    ("0 1 * * *", "foundation.crawler.BusTimeTableCrawler().collect_data()"),
+    ("0 1 * * *", "foundation.crawler.TrainTerminalCrawler().collect_data()"),
+    ("0 1 * * *", "foundation.crawler.TrainTimeTableCrawler().collect_data()"),
+    ("0 1 * * *", "foundation.crawler.SchoolBusTimeTableCrawler().collect_data()"),
+]
